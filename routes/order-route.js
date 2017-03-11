258db38434bc263968a7a70b8dc901a81cc8fab3
nms377 <nms377@nyu.edu>
2017-03-11 11:07:32 -1000

    Create an order route.

---
+++ routes/order-route.js
@@ -0,0 +1,11 @@
+const server = require('../server');
+const express = require('express');
+const models = require('../models');
+
+
+const app = express();
+const router = express.Router();
+
+router.get('/')
+
+module.exports = router;
