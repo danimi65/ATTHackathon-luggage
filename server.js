08b20784f67ccf635d81aa89bf597c36bb77ede1
nms377 <nms377@nyu.edu>
2017-03-11 10:48:42 -1000

    Created server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bp = require('body-parser');
+const app = express();
+const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
+const hbs = handlebars.create({
+	extname: '.hbs',
+	defaultLayout: 'app'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.use(methodOverride('_method'));
+
+const db = require('./models');
+const {Order} = db;
+
+app.listen(3000, function() {
+	console.log('Server listening on port 3000');
+	db.sequelize.sync();
+});
+
+module.exports = app;
